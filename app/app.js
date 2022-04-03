a5f5972ff1249b3da90e65abb167aa9a268bb908
khidirbellz <95444763+khidirbellz@users.noreply.github.com>
2022-04-03 16:48:30 +0400

    Add files via upload

--- app/app.js
+++ app/app.js
@@ -1,1 +1,17 @@
+import Vue from 'nativescript-vue';
 
+import app from "./components/app"
+// Uncommment the following to see NativeScript-Vue output logs
+// Vue.config.silent = false;
+
+new Vue({
+
+    template: `
+        <Frame>
+            <app />
+        </Frame>`,
+
+    components: {
+        app
+    }
+}).$start();
